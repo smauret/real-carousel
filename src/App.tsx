d73bb9d8204c324bda833e6e749b9db094463520
smauret <sarah.mauret@epfedu.fr>
2020-06-19 22:24:51 +0200

    react carousel

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Grid from '@material-ui/core/Grid';
+import Carousel from "./components/Carousel";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <header className="App-header">
+          <Grid container item xs={12}>
+              <Carousel children={['red','yellow','blue','purple']}/>
+          </Grid>
+        </header>
+      </div>
   );
 }
 
