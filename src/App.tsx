a6ad072db5e3259db9d455fc270c65f4ace352f9
smauret <sarah.mauret@epfedu.fr>
2020-06-20 15:47:50 +0200

    added indication of slide index

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,6 @@ import Grid from '@material-ui/core/Grid';
 import Carousel from "./components/Carousel";
 import CarouselCard from "./components/CarouselCard";
 
-interface CarouselCardProps {
-    color: string;
-}
-
 const cards:Array<React.ReactNode> = [
     <CarouselCard color='red'/>,
     <CarouselCard color='yellow'/>,
--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import Grid from '@material-ui/core/Grid';
 import Button from "@material-ui/core/Button";
-
+import Typography from '@material-ui/core/Typography';
 
 interface CarouselProps {
     children: Array<React.ReactNode>;
@@ -10,7 +10,7 @@ interface CarouselProps {
 export default function Carousel({children}: CarouselProps) {
     const [activeIndex, setActiveIndex] = useState(0);
 
-    const subArray = children.slice(activeIndex, activeIndex + 2);
+    const subArray = children.slice(activeIndex, activeIndex + 1);
 
     console.log('activeIndex', activeIndex)
     const goBackward = () => {
@@ -36,6 +36,7 @@ export default function Carousel({children}: CarouselProps) {
             <Grid item xs={1}>
                 <Button variant="contained" onClick={() => goForward()}>Forward</Button>
             </Grid>
+            <Typography>Slide: {activeIndex}</Typography>
         </Grid>
     )
 }
